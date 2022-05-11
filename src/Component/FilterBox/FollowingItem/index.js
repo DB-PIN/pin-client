3bb63c1b36eb56ad2ff4f27914ee66bb8c3bcce4
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2022-05-11 15:38:53 +0900

    init setting

---
+++ src/Component/FilterBox/FollowingItem/index.js
@@ -0,0 +1,11 @@
+/**
+ *  하나의 Following 정보를 나타내는 아이템
+ */
+
+const FollowingItem = () => {
+    return (
+        <></>
+    );
+};
+
+export default FollowingItem;
---
+++ src/Component/FilterBox/index.js
@@ -0,0 +1,11 @@
+/**
+ *  필터 조건들이 보여지는 박스
+ */
+
+const FilterBox = () => {
+    return (
+        <></>
+    )
+}
+
+export default FilterBox;
