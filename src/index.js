144a260af7dd7adc37360a47105cf9b7fd307230
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2022-05-15 23:14:43 +0900

    apply Routing

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Login from './Page/Login';
-import reportWebVitals from './reportWebVitals';
 import {createTheme} from "@mui/material/styles";
 import {ThemeProvider} from "@mui/system";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 const theme = createTheme({
     palette: {
@@ -17,13 +17,11 @@ const theme = createTheme({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ThemeProvider theme={theme}>
-        <React.StrictMode>
-            <Login />
-        </React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Login />} />
+            </Routes>
+        </BrowserRouter>
     </ThemeProvider>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+);
